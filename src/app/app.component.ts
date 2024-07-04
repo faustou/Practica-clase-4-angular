09bf27c6c9cd74f7f2368e1a682574c06fe79766
Faustou <faustoscarmato@hotmail.com>
2024-07-04 00:43:18 -0300

    practica clase 4 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+interface Alumno {
+  nombre: string;
+  estado: 'aprobado' | 'desaprobado';
+  edad: number;
+  nota: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practica-clase-04';
+
+  alumnos: Alumno[] = [
+    {
+      nombre: 'Fausto',
+      estado: 'aprobado',
+      nota: 8,
+      edad: 24
+    },
+    {
+      nombre: 'Donna',
+      estado: 'desaprobado',
+      nota: 4,
+      edad: 3
+    },
+    {
+      nombre: 'Harvey',
+      estado: 'desaprobado',
+      nota: 2,
+      edad: 40
+    },
+    {
+      nombre: 'Mike',
+      estado: 'aprobado',
+      nota: 7,
+      edad: 28
+    },
+  ]
 }
